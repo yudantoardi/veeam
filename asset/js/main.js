811c1f5744e6aad57645b0c8605babe681869189
yudantoardi <yudantoardi@gmail.com>
2022-08-09 17:06:32 +0700

    updates

--- asset/js/main.js
+++ asset/js/main.js
@@ -13,4 +13,30 @@ $(document).ready(function(){
             $(".nav").removeClass("scrolled");
         }
     });
+
+    $(".pop-link").click(function(pl){
+        pl.preventDefault();
+        $($(this).attr("href")).addClass("show");
+    });
+    $(".close-bt").click(function(){
+        $(".overlay").removeClass("show");
+    });
+
+    $(".burger").click(function(){
+        $(".main-menu").toggleClass("show");
+        $(".burger i").toggleClass("fa-bars fa-times")
+    });
+
+    function scrollToSection(event) {
+        event.preventDefault();
+        var $section = $($(this).attr('href')); 
+        $('html, body').animate({
+        scrollTop: $section.offset().top - 60
+        }, 1000);
+    }
+    $('.main-menu a').on('click', scrollToSection);
+    $(".main-menu a").click(function(){
+        $(".main-menu a").removeClass("active");
+        $(this).addClass("active");
+    });
 });
